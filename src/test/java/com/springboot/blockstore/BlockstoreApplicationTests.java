d52ace1058896d2e87a8f09c747738c82137f64a
Fernando Figueredo <fernandofigueredo@ymail.com>
2022-07-22 09:46:47 -0300

    inicial
    commit inicial

---
+++ src/test/java/com/springboot/blockstore/BlockstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.blockstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
